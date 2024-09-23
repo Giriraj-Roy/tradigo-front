bfef7279e964a918070e7fae8db7a368dd494b9b
Giriraj Roy <girirajroy2001@gmail.com>
2024-09-23 11:51:52 +0530

    CoinList UI

--- src/components/Major/CoinList/CoinList.tsx
+++ src/components/Major/CoinList/CoinList.tsx
@@ -1,6 +1,5 @@
-import React, { Suspense, useState } from 'react'
+import React, { Suspense } from 'react'
 import styles from './CoinList.module.css'
-import CoinAttributes from '../../../interfaces/CoinList.interface';
 import CoinOptions from '../../minor/CoinsOptions/CoinOptions';
 import CoinListHeader from '../../minor/CoinListHeader/CoinListHeader';
 import CoinListAll from '../../minor/CoinListAll/CoinListAll';
